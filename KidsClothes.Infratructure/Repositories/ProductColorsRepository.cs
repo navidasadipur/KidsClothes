3e3c3ecd45c6d9fdabc021bbb04029d566e4c03f
User <User@DESKTOP-5IFU6NT>
2021-09-01 12:21:52 +0430

    Repositories/ProductColorsRepo and ProductSizeRepo => add repository

---
+++ KidsClothes.Infratructure/Repositories/ProductColorsRepository.cs
@@ -0,0 +1,30 @@
+﻿using KidsClothes.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidsClothes.Infrastructure.Repositories
+{
+    public class ProductColorsRepository : BaseRepository<ProductColor, MyDbContext>
+    {
+        private readonly MyDbContext _context;
+        private readonly LogsRepository _logger;
+        public ProductColorsRepository(MyDbContext context, LogsRepository logger) : base(context, logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public ProductColor GetProductColor(int id)
+        {
+            return _context.ProductColors.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public ProductColor GetFirstColorByProductId(int productId)
+        {
+            return _context.ProductColors.FirstOrDefault(c => c.IsDeleted == false && c.ProductId == productId);
+        }
+    }
+}
---
+++ KidsClothes.Infratructure/Repositories/ProductSizesRepository.cs
@@ -0,0 +1,30 @@
+﻿using KidsClothes.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidsClothes.Infrastructure.Repositories
+{
+    public class ProductSizesRepository : BaseRepository<ProductColor, MyDbContext>
+    {
+        private readonly MyDbContext _context;
+        private readonly LogsRepository _logger;
+        public ProductSizesRepository(MyDbContext context, LogsRepository logger) : base(context, logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public ProductSize GetProductSize(int id)
+        {
+            return _context.ProductSizes.Where(s => s.Id == id).FirstOrDefault();
+        }
+
+        public ProductSize GetFirstSizeByProductId(int productId)
+        {
+            return _context.ProductSizes.FirstOrDefault(c => c.IsDeleted == false && c.ProductId == productId);
+        }
+    }
+}
